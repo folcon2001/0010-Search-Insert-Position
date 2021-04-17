e440cf217357cbd210a0a2aaca9064ad2863488f
222 111 <folcon2001@gmail.com>
2021-04-17 21:58:01 +0300

    Добавьте файлы проекта.

---
+++ 0010 Search Insert Position/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _0010_Search_Insert_Position
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int[] nums = new int[] { 1};
+            int target = 0;
+
+
+            int SearchInsert(int[] nums, int target)
+            {
+                int k = 0;
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (nums[i] == target)
+                        return i;
+
+                    if (nums[i] > target)
+                        return i;
+
+                    k++;
+                }
+                return k;
+            }
+
+            Console.WriteLine(SearchInsert(nums,target));
+
+
+
+
+
+
+
+
+        }
+    }
+}
